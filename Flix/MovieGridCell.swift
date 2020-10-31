7d8ae8c884dc458145242f1592992c988908dc00
Rinchen Lama <rinchenlama0075@gmail.com>
2020-10-30 20:56:09 -0500

    completed week 3 assignment

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Rinchen on 10/30/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
